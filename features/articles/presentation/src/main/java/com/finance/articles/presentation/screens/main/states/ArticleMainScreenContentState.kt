073c6858a90e30cbccee9e1076cbc42972354bef
igordudka <i.dudka007@gmail.com>
2025-07-01 15:13:11 +0300

    реализовал поиск

--- features/articles/presentation/src/main/java/com/finance/articles/presentation/screens/main/states/ArticleMainScreenContentState.kt
+++ features/articles/presentation/src/main/java/com/finance/articles/presentation/screens/main/states/ArticleMainScreenContentState.kt
@@ -3,13 +3,11 @@ package com.finance.articles.presentation.screens.main.states
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SearchBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import com.finance.articles.domain.models.ArticleCategory
 import com.finance.articles.presentation.screens.main.components.ArticleCategoryCard
 import com.finance.articles.presentation.screens.main.state_hoisting.ArticlesMainScreenAction
@@ -25,13 +23,22 @@ fun ArticlesMainScreenContentState(
 
     var searchQuery by rememberSaveable { mutableStateOf("") }
 
-    com.finance.articles.presentation.screens.main.components.SearchBar(searchQuery, {}) { }
+    com.finance.articles.presentation.screens.main.components.SearchBar(
+        searchQuery,
+        onSearchClick = {},
+        searchExit = {
+            onAction(ArticlesMainScreenAction.OnSearchExit)
+        },
+        onQueryChange = {
+            searchQuery = it
+            onAction(ArticlesMainScreenAction.OnSearchQuery(it.lowercase()))
+        })
 
     LazyColumn {
         item {
             GrayDivider()
         }
-        items(articles){
+        items(articles) {
             ArticleCategoryCard(it.name, it.emoji) { }
             GrayDivider()
         }
